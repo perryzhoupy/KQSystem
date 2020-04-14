af195cc92d9a473533155a606960534221dde36f
perryzhou <42792064+perryzhoupy@users.noreply.github.com>
2020-04-14 11:57:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>考勤系统</title>
+    <title>KQSystem</title>
 </head>
 <body>
-	<center><h1>考勤系统</h1></center>
+	<center><h1>KQSystem</h1></center>
     <form action="submit.php" method="post">
-    	学生姓名：<input type="text" name="name" />
-    	<input type="submit" value="提交" />
+    	Name: <input type="text" name="name" />
+    	<input type="submit" value="Submit" />
     </form>
 	<br />
-    <a href="/admin/"><button>后台管理</button></a>
+    <a href="/admin/"><button>Control Panel</button></a>
 </body>
 </html>
